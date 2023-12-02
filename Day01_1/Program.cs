4f14aa078b14adb439c581d7b856326ece3575a1
Gabriel Dumitru <martesciuc.gabriel@gmail.com>
2023-12-02 17:36:11 +0200

    Added Day01, Problem 1 solution

---
+++ Day01_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+var lines = File.ReadAllLines("input.txt");
+
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var firstDigit = CharUnicodeInfo.GetDecimalDigitValue(line.First(x => Char.IsDigit(x)));
+    var lastDigit = CharUnicodeInfo.GetDecimalDigitValue(line.Last(x => Char.IsDigit(x)));
+
+    sum += firstDigit * 10 + lastDigit;
+}
+
+Console.WriteLine(sum);
